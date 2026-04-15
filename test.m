dbfbd691c19cfa215919fcb431634ccb02fb181c
Noah Schwartz <noah.samuel.schwartz@gmail.com>
2026-04-15 12:05:37 -0600

    Generating a 3-body trajectory and applying SINDy to it. limited success

--- test.m
+++ test.m
@@ -26,3 +26,16 @@ for i=1:length(traj)
 end
 
 save("traj.mat","traj");
+
+
+%%
+
+traj_3body = get_newtonian_trajectories(...
+    [0,0,0; 1 1 1; 2 0 2]',...
+    [0,0,0; 0 0 0; 1 1 0]',...
+    [1,1,3]);
+figure; hold on; grid on;
+for i=1:length(traj)
+    plot3(traj(i).x(1,:),traj(i).x(2,:),traj(i).x(3,:));
+end
+save("3body.mat","traj_3body")
